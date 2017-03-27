ef34c1cc698151779f5ddc939d15589e2181425f
Carl Zulauf <carl@linkleaf.com>
2017-03-27 03:35:45 -0500

    Import rolistic from existing rails app

--- lib/rolistic.rb
+++ lib/rolistic.rb
@@ -1,5 +1,98 @@
 require "rolistic/version"
 
+require "active_support/concern"
+require "active_support/core_ext/module/delegation"
+
 module Rolistic
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  included do
+    attr_reader :name, :abilities
+    delegate :to_s, to: :name
+  end
+
+  def initialize(name = self.class.default)
+    @name = name.to_sym if name
+    @abilities = self.class.abilities_for(@name)
+  end
+
+  def can?(ability)
+    abilities.member?(ability)
+  end
+
+  def default?
+    name == self.class.default
+  end
+
+  def inspect
+    to_s
+  end
+
+  class_methods do
+    def load(raw)
+      case raw
+      when self then raw
+      when String, Symbol
+        new(raw) unless raw.blank?
+      else
+        new
+      end
+    end
+
+    def dump(role)
+      case role
+      when String, Symbol then role.to_s
+      when self
+        role.to_s unless role.default?
+      end
+    end
+
+    def abilities_map
+      return @abilities_map if defined?(@abilities_map)
+      @abilities_map = {}
+    end
+
+    def traits_map
+      return @traits_map if defined?(@traits_map)
+      @traits_map = {}
+    end
+
+    def abilities_for(name)
+      abilities_map.fetch(name) { [] }
+    end
+
+    def abilities_for_trait(trait)
+      traits_map.fetch(trait) { [] }
+    end
+
+    def add_trait(trait, abilities)
+      traits_map[trait] = abilities
+    end
+
+    def add_role(name, *traits_and_abilities, **options)
+      abilities = traits_and_abilities.reduce([]) do |m, t_or_a|
+        case t_or_a
+        when Symbol then m + abilities_for_trait(t_or_a)
+        when Array then m + t_or_a
+        else t_or_a
+        end
+      end
+      abilities_map[name] = abilities
+      @default = name if options[:default]
+    end
+
+    def default
+      @default if defined?(@default)
+    end
+
+    def available
+      abilities_map.keys
+    end
+  end
+
+  module Everything
+    def self.member?(*)
+      true
+    end
+  end
 end
--- spec/rolistic_spec.rb
+++ spec/rolistic_spec.rb
@@ -1,11 +1,83 @@
-require "spec_helper"
+class TestRole
+  include Rolistic
+
+  add_trait :basic_user, %i(manage_account have_fun)
+
+  add_role :n00b, %i(register), default: true
+  add_role :admin, Rolistic::Everything
+  add_role :member, :basic_user
+  add_role :premium_member, :basic_user, %i(access_premium_services)
+  add_role :api_client, %i(access_api)
+  add_role :banned, []
+end
+
+class TestRoleWithoutDefault
+  include Rolistic
+
+  add_role :customer, %i(eat)
+  add_role :cook,     %i(cook)
+end
 
 describe Rolistic do
-  it "has a version number" do
-    expect(Rolistic::VERSION).not_to be nil
+  context "TestRole implementation" do
+    let(:n00b) { TestRole.new }
+    %i(admin member premium_member api_client banned).each do |name|
+      let(name) { TestRole.new(name) }
+    end
+
+    it "acts like a n00b role" do
+      expect(n00b).to be_default
+      expect(n00b.can?(:register)).to be(true)
+    end
+
+    it "acts like a admin role" do
+      expect(admin).not_to be_default
+      expect(admin.can?(:access_api))
+      expect(admin.can?(:do_anything)).to be(true)
+    end
+
+    it "acts like a member role" do
+      expect(member.can?(:manage_account)).to be(true)
+      expect(member.can?(:anything_else)).to be(false)
+    end
+
+    it "acts like a premium member role" do
+      expect(premium_member.can?(:have_fun)).to be(true)
+      expect(premium_member.can?(:access_premium_services)).to be(true)
+      expect(premium_member.can?(:other_stuff)).to be(false)
+    end
+
+    it "acts like a api_client role" do
+      expect(api_client.can?(:access_api)).to be(true)
+      expect(api_client.can?(:manage_account)).to be(false)
+    end
+
+    it "acts like a banned role" do
+      expect(banned.can?(:access_api)).to be(false)
+      expect(banned.can?(:manage_account)).to be(false)
+      expect(banned.can?(:reticulate_splines)).to be(false)
+    end
   end
 
-  it "is a module" do
-    expect(subject.class).to eq(Module)
+  context "TestRoleWithoutDefault implementation" do
+    describe "new" do
+      it "does not error when given nil" do
+        TestRoleWithoutDefault.new
+        TestRoleWithoutDefault.new(nil)
+      end
+
+      it "treats nil role as default" do
+        role = TestRoleWithoutDefault.new
+        expect(role).to be_default
+      end
+
+      it "works with non-default roles still" do
+        role = TestRoleWithoutDefault.new(:customer)
+        expect(role).not_to be_default
+        expect(role.can?(:eat)).to be(true)
+        expect(role.can?(:cook)).to be(false)
+        expect(role.can?(:random_thing)).to be(false)
+      end
+    end
   end
 end
